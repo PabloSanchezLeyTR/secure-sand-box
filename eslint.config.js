60404ee50238867881a71eb8c47b423c2f9cc708
Pablo <Pablo.Sanchez2@thomsonreuters.com>
2025-08-30 20:44:49 -0600

    slint congif

---
+++ eslint.config.js
@@ -0,0 +1,42 @@
+import { FlatCompat } from '@eslint/eslintrc';
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+export default [
+  ...compat.config({
+    root: true,
+    ignorePatterns: ['**/*.js'],
+    overrides: [
+      {
+        files: ['*.ts'],
+        parserOptions: {
+          project: ['tsconfig.json'],
+          createDefaultProgram: true,
+        },
+        extends: [
+          'eslint:recommended',
+          'plugin:@typescript-eslint/recommended',
+          'plugin:@angular-eslint/recommended',
+        ],
+        plugins: ['@typescript-eslint'],
+        rules: {
+          '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+          '@typescript-eslint/no-explicit-any': 'warn',
+          '@typescript-eslint/explicit-function-return-type': 'off',
+          'no-console': 'warn',
+        },
+      },
+      {
+        files: ['*.html'],
+        extends: ['plugin:@angular-eslint/template/recommended'],
+        plugins: ['@angular-eslint/template'],
+        rules: {
+          '@angular-eslint/template/no-negated-async': 'error',
+          '@angular-eslint/template/accessible-name': 'warn',
+        },
+      },
+    ],
+  }),
+];
