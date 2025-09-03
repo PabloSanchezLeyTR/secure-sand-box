9a3fbbe93aad6fc9d77004850a5d40e3e976fe55
Pablo <Pablo.Sanchez2@thomsonreuters.com>
2025-09-02 18:34:19 -0600

    test errores

--- pablo-sand-box/src/app/app.component.ts
+++ pablo-sand-box/src/app/app.component.ts
@@ -1,3 +1,5 @@
+const unusedVariable = 123;
+var nombre = "Pablo";
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pablo-sand-box';
+
+  saludar() {
+    console.log("Hola");
+    let nombre_usuario = "Pablo";
+
+    console.log("Hola mundo")
+  }
+
+
+ sinUso() {
+  return true;
+}
+
 }
