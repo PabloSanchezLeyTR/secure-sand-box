dc27fec83f0b046a5557c80a92351949aaca1dd0
Pablo <Pablo.Sanchez2@thomsonreuters.com>
2025-09-04 11:19:20 -0600

    create errors

--- pablo-sand-box/src/app/app.component.ts
+++ pablo-sand-box/src/app/app.component.ts
@@ -3,18 +3,37 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <!-- Basic static accessibility errors for axe-linter testing -->
-    <button></button> <!-- Empty button -->
-    <img src="assets/logo.png"> <!-- Image without alt -->
-    <div style="color: #ccc; background-color: #eee;">Low contrast text</div>
-    <a href="#">Click here</a> <!-- Link with generic text -->
-    <h3>Subheading without h2</h3> <!-- Heading structure error -->
-    <input type="text" id="name"> <!-- Input without label -->
-    <div id="duplicate"></div><div id="duplicate"></div> <!-- Non-unique IDs -->
-    <table><tr><td>Data</td></tr></table> <!-- Table without headers -->
+    <!-- Static HTML accessibility errors for axe-linter testing -->
+    <button></button>
+    <img src='logo.png'>
+    <input type='text'>
+    <a></a>
+    <a href='#'></a>
+    <font color='red'>Deprecated text</font>
+    <table><tr><td>Data</td></tr></table>
+    <span style='color: #eee; background: #fff;'>Low contrast text</span>
+    <iframe src='video.mp4'></iframe>
+    <div id='duplicate'></div>
+    <div id='duplicate'></div>
+    <ul></ul>
   `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'pablo-sand-box';
+  // Static HTML accessibility errors for axe-linter testing
+  template = `
+    <button></button>
+    <img src='logo.png'>
+    <input type='text'>
+    <a></a>
+    <a href='#'></a>
+    <font color='red'>Deprecated text</font>
+    <table><tr><td>Data</td></tr></table>
+    <span style='color: #eee; background: #fff;'>Low contrast text</span>
+    <iframe src='video.mp4'></iframe>
+    <div id='duplicate'></div>
+    <div id='duplicate'></div>
+    <ul></ul>
+  `;
 }
