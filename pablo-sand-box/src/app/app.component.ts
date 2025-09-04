6d75b0557225c81361da40755dbb89e302722baa
Pablo <Pablo.Sanchez2@thomsonreuters.com>
2025-09-04 11:27:03 -0600

    axe-lint sin errores

--- pablo-sand-box/src/app/app.component.ts
+++ pablo-sand-box/src/app/app.component.ts
@@ -2,38 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <!-- Static HTML accessibility errors for axe-linter testing -->
-    <button></button>
-    <img src='logo.png'>
-    <input type='text'>
-    <a></a>
-    <a href='#'></a>
-    <font color='red'>Deprecated text</font>
-    <table><tr><td>Data</td></tr></table>
-    <span style='color: #eee; background: #fff;'>Low contrast text</span>
-    <iframe src='video.mp4'></iframe>
-    <div id='duplicate'></div>
-    <div id='duplicate'></div>
-    <ul></ul>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'pablo-sand-box';
-  // Static HTML accessibility errors for axe-linter testing
-  template = `
-    <button></button>
-    <img src='logo.png'>
-    <input type='text'>
-    <a></a>
-    <a href='#'></a>
-    <font color='red'>Deprecated text</font>
-    <table><tr><td>Data</td></tr></table>
-    <span style='color: #eee; background: #fff;'>Low contrast text</span>
-    <iframe src='video.mp4'></iframe>
-    <div id='duplicate'></div>
-    <div id='duplicate'></div>
-    <ul></ul>
-  `;
 }
