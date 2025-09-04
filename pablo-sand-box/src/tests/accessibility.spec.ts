0ded4bbf194369bf3b8eff1be55eee01c7977f88
Pablo <Pablo.Sanchez2@thomsonreuters.com>
2025-09-04 13:48:08 -0600

    fix

--- pablo-sand-box/src/tests/accessibility.spec.ts
+++ pablo-sand-box/src/tests/accessibility.spec.ts
@@ -1,11 +1,8 @@
 import { test, expect } from '@playwright/test';
-import { injectAxe, checkA11y } from '@axe-core/playwright';
+import AxeBuilder from '@axe-core/playwright';
 
 test('accessibility scan', async ({ page }) => {
   await page.goto('http://localhost:4200');
-  await injectAxe(page);
-  await checkA11y(page, null, {
-    detailedReport: true,
-    detailedReportOptions: { html: true }
-  });
+  const results = await new AxeBuilder({ page }).analyze();
+  expect(results.violations).toEqual([]);
 });
