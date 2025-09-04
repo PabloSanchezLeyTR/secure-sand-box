90539a16f6527bb77f6a4cccfafa40bceee0ada1
Pablo <Pablo.Sanchez2@thomsonreuters.com>
2025-09-04 17:44:01 -0600

    test completo

--- pablo-sand-box/src/app/app.component.ts
+++ pablo-sand-box/src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pablo-sand-box';
+
+  // HTML con errores de accesibilidad para pruebas
+  a11yErrorsHtml = `
+    <!-- Botón sin nombre accesible -->
+    <button></button>
+    <!-- Imagen sin alt -->
+    <img src=\"logo.png\">
+    <!-- Input sin label -->
+    <input type=\"text\">
+    <!-- Enlace vacío -->
+    <a></a>
+    <a href=\"#\"></a>
+    <!-- Elemento deprecado -->
+    <font color=\"red\">Texto deprecado</font>
+    <!-- Tabla sin encabezados -->
+    <table><tr><td>Dato</td></tr></table>
+    <!-- Bajo contraste -->
+    <span style=\"color: #eee; background: #fff;\">Texto bajo contraste</span>
+    <!-- Iframe sin título -->
+    <iframe src=\"video.mp4\"></iframe>
+    <!-- IDs duplicados -->
+    <div id=\"duplicado\"></div><div id=\"duplicado\"></div>
+    <!-- Lista vacía -->
+    <ul></ul>
+  `;
 }
