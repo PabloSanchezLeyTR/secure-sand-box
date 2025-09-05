654ac764982eb961cc3097caf9010669a51a2985
Pablo <Pablo.Sanchez2@thomsonreuters.com>
2025-09-05 12:20:20 -0600

    anotaciones

--- pablo-sand-box/src/app/app.component.ts
+++ pablo-sand-box/src/app/app.component.ts
@@ -2,22 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  // Template inline con errores de accesibilidad intencionales
-  template: `
-    <img src="logo.png">
-    <button></button>
-    <a href="#"></a>
-    <input type="text">
-    <form>
-      <input type="text">
-    </form>
-    <span style="color:#ccc; background:#fff">Texto sin contraste</span>
-    <iframe src="about:blank"></iframe>
-    <div id="duplicado"></div>
-    <div id="duplicado"></div>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'pablo-sand-box';
+
+  // Cambio artificial para pruebas de PR
+  getVersion(): string {
+    return '1.0.0';
+  }
 }
