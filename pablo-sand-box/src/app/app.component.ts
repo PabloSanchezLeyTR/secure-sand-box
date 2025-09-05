0aa62a531ad4c991ef14a2f6eb9908e29fd38538
Pablo <Pablo.Sanchez2@thomsonreuters.com>
2025-09-05 12:01:48 -0600

    test ts

--- pablo-sand-box/src/app/app.component.ts
+++ pablo-sand-box/src/app/app.component.ts
@@ -2,7 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // Template inline con errores de accesibilidad intencionales
+  template: `
+    <img src="logo.png">
+    <button></button>
+    <a href="#"></a>
+    <input type="text">
+    <form>
+      <input type="text">
+    </form>
+    <span style="color:#ccc; background:#fff">Texto sin contraste</span>
+    <iframe src="about:blank"></iframe>
+    <div id="duplicado"></div>
+    <div id="duplicado"></div>
+  `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
