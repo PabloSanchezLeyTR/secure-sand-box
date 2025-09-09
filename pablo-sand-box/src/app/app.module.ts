7dd95c967c24497bb6d9dd6e2a9d14cd30bd5de9
Pablo <Pablo.Sanchez2@thomsonreuters.com>
2025-09-09 08:19:19 -0600

    adding saffron

--- pablo-sand-box/src/app/app.module.ts
+++ pablo-sand-box/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   declarations: [
     AppComponent
   ],
