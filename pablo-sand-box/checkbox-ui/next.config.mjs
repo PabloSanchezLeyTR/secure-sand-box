eb97bf3d57afd1e69c6ab9e57a9309333638ba2a
Pablo <Pablo.Sanchez2@thomsonreuters.com>
2025-09-26 11:48:09 -0600

    prefix

--- pablo-sand-box/checkbox-ui/next.config.mjs
+++ pablo-sand-box/checkbox-ui/next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/secure-sand-box/checkbox-ui',
+  assetPrefix: '/secure-sand-box/checkbox-ui',
   eslint: {
     ignoreDuringBuilds: true,
   },
