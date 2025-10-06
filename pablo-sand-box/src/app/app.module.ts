aa2a81681375fc911888af8bc97ca1a59fa44bb8
Pablo <Pablo.Sanchez2@thomsonreuters.com>
2025-10-06 11:19:02 -0600

    remove paella

--- pablo-sand-box/src/app/app.module.ts
+++ pablo-sand-box/src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SaffronImplementation100Component } from './saffron-implementation-100.component';
 
 @NgModule({
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   declarations: [
     AppComponent,
-    SaffronImplementation100Component
   ],
   imports: [
     BrowserModule,
